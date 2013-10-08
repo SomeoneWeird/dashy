023b778e2657b00993dd29265f263e9f606440d0
Adam Brady <adam@volatiletechnology.com>
2013-10-08 14:26:12 +1100

    clean up

--- devserver.js
+++ devserver.js
@@ -1,9 +1,33 @@
-#!/usr/bin/node
-var express = require('express'), app = express();
-app.set('view options', {layout: false});
-app.use(express.logger());
-app.use(app.router);
-app.use(express.static(__dirname));
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-app.get('/', function(req, res) { res.sendfile('index.html') });
-app.listen(8000);
+#!/usr/bin/env node
+var express = require('express');
+
+var app = express();
+
+app.configure(function() {
+
+	app.set('view options', {
+		layout: false
+	});
+
+	app.use(express.logger());
+	app.use(app.router);
+	app.use(express.static(__dirname));
+
+	app.use(express.errorHandler({ 
+		dumpExceptions: true, 
+		showStack:      true 
+	}));
+
+});
+
+app.get('/', function(req, res) { 
+
+	res.sendfile('index.html');
+
+});
+
+app.listen(8000, function() {
+
+	console.log("Started on port 8000");
+
+});
